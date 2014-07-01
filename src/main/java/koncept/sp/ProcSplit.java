0f04fe45405cbaae14246a364f02b3d175f1796e
nkrul <nicholas.krul@gmail.com>
2014-07-01 23:28:47 +0100

    Add a resource ordering to the proc split - a stack

--- src/main/java/koncept/sp/ProcSplit.java
+++ src/main/java/koncept/sp/ProcSplit.java
@@ -1,22 +1,21 @@
 package koncept.sp;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import koncept.sp.resource.CleanableResource;
 
 public class ProcSplit {
 	public static final String DEFAULT_VALUE_KEY = "value";
-	private final Map<String, CleanableResource> cleanableResources;
+	private final List<NamedCleanableResource> namedCleanableResources;
 
 	public ProcSplit() {
-		this(new HashMap<String, CleanableResource>());
+		this(new LinkedList<NamedCleanableResource>());
 	}
 	
 	public ProcSplit(ProcSplit previousStep) {
-		this(new HashMap<String, CleanableResource>(previousStep.cleanableResources));
+		this(new LinkedList<NamedCleanableResource>(previousStep.namedCleanableResources));
 	}
 	
 	public ProcSplit(CleanableResource value) {
@@ -24,41 +23,70 @@ public class ProcSplit {
 		add(DEFAULT_VALUE_KEY, value);
 	}
 	
-	private ProcSplit(Map<String, CleanableResource> cleanableResources) {
-		this.cleanableResources = cleanableResources;
+	private ProcSplit(List<NamedCleanableResource> cleanableResources) {
+		this.namedCleanableResources = cleanableResources;
 	}
 	
 	public CleanableResource getCleanableResource(String name) {
-		return cleanableResources.get(name);
+		for(NamedCleanableResource namedCleanableResource: namedCleanableResources)
+			if (namedCleanableResource.name.equals(name))
+				return namedCleanableResource.cleanableResources;
+		return null;
 	}
 	
 	public CleanableResource removeCleanableResource(String name) {
-		return cleanableResources.remove(name);
+		for(NamedCleanableResource namedCleanableResource: namedCleanableResources)
+			if (namedCleanableResource.name.equals(name)) {
+				namedCleanableResources.remove(namedCleanableResource);
+				return namedCleanableResource.cleanableResources;
+			}
+		return null;
 	}
 	
+	//push it to the front - the default cleanup will clean newer resources first (its a stack)
 	public ProcSplit add(String name, CleanableResource cleanableResource) {
-		cleanableResources.put(name, cleanableResource);
+		namedCleanableResources.add(0, new NamedCleanableResource(name, cleanableResource));
 		return this;
 	}
 	
-	public Object get(String name) {
-		CleanableResource cleanableResource = cleanableResources.get(name);
+	public Object getResource(String name) {
+		CleanableResource cleanableResource = getCleanableResource(name);
 		return cleanableResource == null ? null : cleanableResource.get();
 	}
 	
 	public ProcSplit clean(String name) {
-		CleanableResource cleanableResource = cleanableResources.remove(name);
+		CleanableResource cleanableResource = removeCleanableResource(name);
 		if (cleanableResource != null)
 			cleanableResource.clean();
 		return this;
 	}
 	
-	public Collection<String> getResourceNames() {
-		return new HashSet<String>(cleanableResources.keySet());
+	public ProcSplit clean() {
+		for(NamedCleanableResource namedCleanableResource: namedCleanableResources)
+			namedCleanableResource.cleanableResources.clean();
+		namedCleanableResources.clear();
+		return this;
+	}
+	
+	public List<String> getResourceNames() {
+		List<String> names = new ArrayList<>(namedCleanableResources.size());
+		for(NamedCleanableResource namedCleanableResource: namedCleanableResources)
+			names.add(namedCleanableResource.name);
+		return names;
 	}
 
 	@Override
 	public String toString() {
-		return cleanableResources.toString();
+		return namedCleanableResources.toString();
+	}
+	
+	private static class NamedCleanableResource {
+		private final String name;
+		private final CleanableResource cleanableResources;
+		
+		public NamedCleanableResource(String name, CleanableResource cleanableResources) {
+			this.name = name;
+			this.cleanableResources = cleanableResources;
+		}
 	}
 }
