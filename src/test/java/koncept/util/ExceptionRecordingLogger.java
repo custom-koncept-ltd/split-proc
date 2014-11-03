3d041c216902033f56688b13852c0ebf56b43b63
nicholas Krul <nicholas.krul@gmail.com>
2014-11-03 22:19:00 +0000

    synchronize recording test logger

--- src/test/java/koncept/util/ExceptionRecordingLogger.java
+++ src/test/java/koncept/util/ExceptionRecordingLogger.java
@@ -2,12 +2,13 @@ package koncept.util;
 
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ExceptionRecordingLogger extends Logger {
-	public final List<LogMessage> logs = new ArrayList<>();
+	public final List<LogMessage> logs = Collections.synchronizedList(new ArrayList<LogMessage>());
 	
 	public ExceptionRecordingLogger() {
 		this(ExceptionRecordingLogger.class.getName());
